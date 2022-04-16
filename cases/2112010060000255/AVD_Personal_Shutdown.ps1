4e0052a4d8f8d2aab8ea435a3858580b743d94f3
ripenedcat <43979954+ripenedcat@users.noreply.github.com>
2022-04-17 07:52:38 +0800

    Update and rename AVDUserSession.ps1 to AVD_Personal_Shutdown.ps1

--- cases/2112010060000255/AVD_Personal_Shutdown.ps1
+++ cases/2112010060000255/AVD_Personal_Shutdown.ps1
@@ -1,14 +1,21 @@
+
+#==============Parameters=================
 #your environment information here.
 $ResourceGroupName = "TinaSub_WVD"
 $HostPoolName = "Personal"
-#Put exclusion VM Name (without domain) in the list 
-$VMExclusionList = ,"Per-0"
-#$ADGroupExclusionIDList= "","2ca0dd39-8195-4531-9e7a-95a5e21bb403"
-$AADGroupExclusionNameList= ,"AAD add gp","Computer_aadgp"
+#Put exclusion VM Name (without domain) in the list. This is for some VMs which are not in a AAD Group, but still want to be excluded to be turned off. 
+$VMExclusionList = ,""
+#Put AAD Group name here. all VMs in this AAD Group will not be turned off.
+$AADGroupExclusionNameList= ,""
+#=========================================
+
+
+$errorActionPreference = "Stop"
+# Initialize an empty variable for transfer AAD group name to id 
 $AADGroupExclusionIDList= @()
-
-
-#==Azure Login==
+# Initialize an empty array to record VM turned off by this script.
+$DeallocatedVMList= @()
+#==Section of Azure Login==
 $ConnectionAsset = Get-AutomationConnection -Name "AzureRunAsConnection"
 $AzContext = $null
 try {
@@ -22,8 +29,8 @@ catch {
     throw [System.Exception]::new('Failed to authenticate Azure with application ID, tenant ID, subscription ID', $PSItem.Exception)
 }
 Write-Output "Successfully authenticated with Azure using service principal: $($AzContext | Format-List -Force | Out-String)"
-
-#==Azure AD Login==
+#===============================
+#==Section of Azure AD Login==
 try {
     $AzureADAuth = Connect-AzureAD -ApplicationId $ConnectionAsset.ApplicationId -CertificateThumbprint $ConnectionAsset.CertificateThumbprint -TenantId $ConnectionAsset.TenantId
 }
@@ -31,15 +38,18 @@ catch {
     throw [System.Exception]::new('Failed to authenticate Azure AD with application ID, tenant ID, subscription ID', $PSItem.Exception)
 }
 Write-Output "Successfully connected to Azure AD"
+#==================================
 
-#==Adding AAD Group Exception VMs to $VMExclusionList
+
+#==Add AAD Group Exception VMs to $VMExclusionList
 foreach($AADGroup in $AADGroupExclusionNameList){
+	if([String]::IsNullOrEmpty($AADGroup)){
+		continue
+	}
 	$filter = "DisplayName eq '"+ $AADGroup +"'"
 	$AADGroupObjectId = (Get-AzureADGroup -Filter $filter).ObjectId
 	$AADGroupExclusionIDList+=$AADGroupObjectId
 }
-
-
 foreach($AADGroupExclusionID in $AADGroupExclusionIDList){
 	if($AADGroupExclusionID -eq ""){
 		continue
@@ -50,27 +60,35 @@ foreach($AADGroupExclusionID in $AADGroupExclusionIDList){
 	}
 }
 Write-Output("Adding Single VMs exception and AAD group exception together, the Exception VMs are $VMExclusionList")
+#============================
 
-Write-Output("Start Scanning Hosts")
+
+Write-Output("Start Scanning Hosts in verbose")
 $SessionHosts = Get-AzWvdSessionHost -ResourceGroupName $ResourceGroupName -HostPoolName $HostPoolName
-
+#Iterate all VMs in a session Hosts
 foreach($SessionHost in $SessionHosts){
-	$hostName = ($SessionHost.Name -split "/")[1]
 	
+	$hostName = ($SessionHost.Name -split "/")[1]
+	#get current user session
 	$userSession = Get-AzWvdUserSession -ResourceGroupName $ResourceGroupName -HostPoolName $HostPoolName -SessionHostName $hostName
-	$shutdownFlag = $true
+	#get current user session count
 	$sessionCount = ($userSession | measure).count
+	# shutdownFlag by default is true. true means VM will be turned off
+	$shutdownFlag = $true
+	# initialize sessionState as N/A when there is no current user Session
 	$sessionState = "N/A"
 	if ($sessionCount -gt 0){
+		#get real session state. if there is a session with Active or Disconnected state, set $shutdownFlag to false and VM will not be turned off
 		$sessionState = $userSession.SessionState
-		if (($sessionState -eq "Active") -or ($sessionState -eq "Disconnected")){
-			#if (($sessionState -eq "Active")){
+		if (($sessionState -eq "Active") -or ($sessionState -eq "Disconnected")){			
 			$shutdownFlag = $false
 		}
 	}
-	Write-Output "The session host $hostName has $sessionCount session, its state is $sessionState, will be turned off with $shutdownFlag."
+	Write-Output "The session host $hostName has $sessionCount session, session state is $sessionState, will be turned off with $shutdownFlag."
+	# check if this VM will be shutdown
 	if ($shutdownFlag){
 		$VMName = ($hostName -split "\.")[0]
+		#if the VM is in VMExclusionList, skip turning off. or will start deallocating
 		if($VMExclusionList -contains $VMName){
 			Write-Output "$VMName is in VMExclusionList, will skip deallocate"
 			continue
@@ -78,12 +96,13 @@ foreach($SessionHost in $SessionHosts){
 			Write-Output "$VMName is not in VMExclusionList, will start deallocation"
 		}
 		try{
-			$VMStatus = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Status
-			#$b.Statuses
+			# check current VM Status, if already deallocated, no need to deallocate again
+			$VMStatus = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Status			
 			if ($VMStatus.Statuses.Code -contains ("PowerState/deallocated")){
 				Write-Output "$VMName is already deallocated, skip"
 			}else{
 				$a = Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Force -NoWait
+				$DeallocatedVMList += $VMName
 				Write-Output "VM $VMName is being deallocated."
 			}
 			
@@ -94,6 +113,11 @@ foreach($SessionHost in $SessionHosts){
 	}
 
 }
-
-Write-Output "Finished"
+# Output Summary
+Write-Output "Script Finished."
+if ( $DeallocatedVMList.count -eq 0){
+	Write-Output "In summarize, no VM is turned off by this script."
+}else{
+	Write-Output "In summarize, VMs turned off by this script are $DeallocatedVMList"
+}
 
